fe9e490d65c4b3c71d0ca4e2e25ef983de1ed853
abudvig <abudvig@precognox.com>
2017-07-04 15:56:29 +0200

    Added all methods in Task class

--- src/tlog16java/Task.java
+++ src/tlog16java/Task.java
@@ -5,15 +5,90 @@
  */
 package tlog16java;
 
-import java.time.LocalDate;
+import java.time.LocalTime;
 
 /**
  *
  * @author Szandi
  */
 public class Task {
+
     String taskId;
-    LocalDate startTime;
-    LocalDate endTime;
+    LocalTime startTime;
+    LocalTime endTime;
     String comment;
+
+    public Task(String taskId, String comment, int startHour, int startMin, int endHour, int endMin) {
+        this.taskId = taskId;
+        this.comment = comment;
+
+        startTime = LocalTime.of(startHour, startMin);
+        endTime = LocalTime.of(endHour, endMin);
+    }
+
+    public Task(String taskId, String comment, String startTime, String endTime) {
+        this.taskId = taskId;
+        this.comment = comment;
+
+        int startHour = Integer.parseInt(startTime.substring(0, 2));
+        int startMinute = Integer.parseInt(startTime.substring(3));
+        this.startTime = LocalTime.of(startHour, startMinute);
+
+        int endHour = Integer.parseInt(endTime.substring(0, 2));
+        int endMinute = Integer.parseInt(endTime.substring(3));
+        this.endTime = LocalTime.of(endHour, endMinute);
+    }
+
+    public String getTaskId() {
+        return taskId;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    long getMinPerTask() {
+        long startTimeInMinutes = (startTime.getHour()) * 60 + startTime.getMinute();
+        long endTimeInMinutes = (endTime.getHour()) * 60 + endTime.getMinute();
+        long MinPerTask = endTimeInMinutes - startTimeInMinutes;
+        return MinPerTask;
+    }
+
+    boolean isValidTaskId() {
+        int digitCounter = 0;
+        boolean isValidTaskIdValue = false;
+
+        for (char c : taskId.toCharArray()) {
+            if (c >= '0' && c <= '9') {
+                ++digitCounter;
+            }
+        }
+
+        if (digitCounter == 4) {
+            if ((taskId.length() == 4) || (taskId.startsWith("LT-") && taskId.length() == 7)) {
+                isValidTaskIdValue = true;
+            }
+        }
+
+        return isValidTaskIdValue;
+    }
+
+    boolean isMultipleQuarterHour() {
+        boolean isMultipleQuarterHourValue = false;
+        long time = this.getMinPerTask();
+
+        if (time % 15 == 0) {
+            isMultipleQuarterHourValue = true;
+        }
+
+        return isMultipleQuarterHourValue;
+    }
 }
